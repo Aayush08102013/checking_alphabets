055bc8a492bf7d2b07c0ec45d0990f06054ada94
Aayush08102013 <aayush.ranjan0815@gmail.com>
2025-03-14 17:37:50 +1300

    Add files via upload

---
+++ checking_alphabets.py
@@ -0,0 +1,73 @@
+alpha = input("enter your charecter to be checked if its in the alphabet: ")
+#a
+if alpha == "a"or "A":
+    print (f"{alpha} is in the alphabet")
+#b
+elif alpha == "b" or "B":
+    print (f"{alpha} is in the alphabet")
+#c
+elif alpha == "c" or "C":
+    print (f"{alpha} is in the alphabet")
+#d
+elif alpha == "d" or "D":
+    print (f"{alpha} is in the alphabet")
+#e
+elif alpha == "e" or "E":
+    print (f"{alpha} is in the alphabet")
+#f
+elif alpha == "f" or "F":
+    print (f"{alpha} is in the alphabet")
+#g
+elif alpha == "g" or "G":
+    print (f"{alpha} is in the alphabet")
+#h
+elif alpha == "h" or "H":
+    print (f"{alpha} is in the alphabet")
+#i
+elif alpha == "i" or "I":
+    print (f"{alpha} is in the alphabet")
+#l
+elif alpha == "l" or "L":
+    print (f"{alpha} is in the alphabet")
+#m
+elif alpha == "m" or "M":
+    print (f"{alpha} is in the alphabet")
+#n
+elif alpha == "n" or "N":
+    print (f"{alpha} is in the alphabet")
+#o
+elif alpha == "o" or "O":
+    print (f"{alpha} is in the alphabet")
+#p
+elif alpha == "p" or "P":
+    print (f"{alpha} is in the alphabet")
+#q
+elif alpha == "q" or "Q":
+    print (f"{alpha} is in the alphabet")
+#r
+elif alpha == "r" or "R":
+    print (f"{alpha} is in the alphabet")
+#s
+elif alpha == "s" or "S":
+    print (f"{alpha} is in the alphabet")
+#t
+elif alpha == "t" or "T":
+    print (f"{alpha} is in the alphabet")
+#u
+elif alpha == "u" or "U":
+    print (f"{alpha} is in the alphabet")
+#v
+elif alpha == "v" or "V":
+    print (f"{alpha} is in the alphabet")
+#w
+elif alpha == "w" or "W":
+    print (f"{alpha} is in the alphabet")
+#x
+elif alpha == "x" or "X":
+    print (f"{alpha} is in the alphabet")
+#y
+elif alpha == "y" or "Y":
+    print (f"{alpha} is in the alphabet")
+#z
+else:
+    print (f"{alpha} is in the alphabet")
